d24b2703b190c1aeef9e349eb76e6354a1ae1d7e
Ubuntu <ubuntu@ip-172-31-47-102.us-west-2.compute.internal>
2016-11-30 20:42:38 +0000

    Views

--- lswa/musician/views.py
+++ lswa/musician/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def index(request):
