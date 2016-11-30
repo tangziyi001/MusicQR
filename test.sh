15b8ab55a297ede6030e8e6bc92c3491c069f37e
Charlespartina <tangziyi001@gmail.com>
2016-11-30 00:25:20 -0500

    Remove Scalica

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo 'Go to http://35.163.220.222:8000/micro/'
-source ./env/bin/activate && python ./web/scalica/manage.py runserver 0.0.0.0:8000
--- web/scalica/utils/misc.py
+++
@@ -1,5 +0,0 @@
-#utils/misc.py
-
-def custom_show_toolbar(request):
-  return False
-  #return "1" == request.COOKIES.get('DebugToolbar', False)
