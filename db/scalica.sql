7632d2ee566ded34efcda986e490a7605ce38c5f
Charlespartina <tangziyi001@gmail.com>
2016-11-30 00:26:23 -0500

    Remove Scalica DB

--- db/scalica.sql
+++
@@ -1,8 +0,0 @@
-/* Create our database */
-CREATE DATABASE scalica CHARACTER SET utf8;
-
-/* Setup permissions for the server */
-CREATE USER 'appserver'@'localhost' IDENTIFIED BY 'foobarzoot';
-CREATE USER 'www-data'@'localhost' IDENTIFIED BY 'foobarzoot';
-GRANT ALL ON scalica.* TO 'appserver'@'localhost';
-GRANT ALL ON scalica.* TO 'www-data'@'localhost';
