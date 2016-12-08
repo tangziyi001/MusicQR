2d687dfb399ab6486d34bd3a23fe7e8e753038eb
Charlespartina <tangziyi001@gmail.com>
2016-12-08 16:31:58 +0000

    Update

--- backend/ranking/aggregate_daily_ranking.cpp
+++ backend/ranking/aggregate_daily_ranking.cpp
@@ -50,6 +50,7 @@ int main(int argc, const char **argv)
       con->setSchema("musician");
       stmt = con->createStatement();
       /* Get Data After Grouping */
+      cout << curDate() << endl;
       string com = "SELECT music_id, count FROM (SELECT d.id as music_id, IFNULL(m.count,0) AS count FROM (SELECT music_id, COUNT(*) AS count FROM musician_download WHERE CAST(download_time AS DATE) = '"+curDate()+"' GROUP BY music_id) AS m RIGHT OUTER JOIN musician_music AS d ON m.music_id=d.id) AS g ORDER BY g.count DESC"; 
       res = stmt->executeQuery(com);
       /* Ranking */
